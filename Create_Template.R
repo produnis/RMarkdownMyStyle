50d414b5b27ffc0c76351de80dfea4e179710b08
Joe Slam <schlarmann@produnis.de>
2021-04-10 20:43:01 +0200

    template meineRechnung

--- Create_Template.R
+++ Create_Template.R
@@ -3,10 +3,19 @@
 
 install.packages("usethis", dependencies = T)
 
+# erzeuge die Dateiordnerstruktur für "meinBrief"
 usethis::use_rmarkdown_template(
   template_name = "meinBrief",
   template_dir = NULL,
   template_description = "Meine private Briefvorlage",
+  template_create_dir = FALSE
+)
+
+# erzeuge die Dateiordnerstruktur für "meineRechnung"
+usethis::use_rmarkdown_template(
+  template_name = "meineRechnung",
+  template_dir = NULL,
+  template_description = "Meine private Rechnungsvorlage",
   template_create_dir = FALSE
 )
 
