3073d0d4413eafe7afb73802ea1c8354a8463779
Joe Slam <schlarmann@produnis.de>
2021-04-10 19:15:03 +0200

    some polishing

--- R/formats.R
+++ R/formats.R
@@ -1,5 +1,15 @@
+# mache output "meinbrief" verfügbar
 meinbrief <- function(...) {
   template <- system.file("rmarkdown/templates/meinbrief/resources/meinbrief.tex",
+                          package="MyStyle")
+  bookdown::pdf_document2(...,
+                          template = template
+  )
+}
+
+# mache output "meinerechnung" verfügbar
+meinerechnung <- function(...) {
+  template <- system.file("rmarkdown/templates/meinerechnung/resources/meinerechnung.tex",
                           package="MyStyle")
   bookdown::pdf_document2(...,
                           template = template
